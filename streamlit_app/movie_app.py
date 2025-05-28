584861db6828daba5603947be636aecd3bb1a492
Isabelle LACMAGO <isabelle.lacmago@gmail.com>
2025-05-28 15:41:05 +0200

    Page principale de l'application

--- streamlit_app/movie_app.py
+++ streamlit_app/movie_app.py
@@ -1,14 +1,11 @@
 import streamlit as st
 
-st.set_page_config(
-   layout="wide",
-   page_title= "Analyse des données de notre API MovieLenz",
-   page_icon= "🎞️"
-)
+
 # Navigation
+page_0 = st.Page("page0.py", title="Home", icon="🏠")         # House
 page_1 = st.Page("page1.py", title="Overview", icon="🎬")     # Film clapperboard
 page_2 = st.Page("page2.py", title="Tags Insights", icon="📊")  # Bar chart
 page_3 = st.Page("page3.py", title="Movie Explorer", icon="🔎") # Magnifying glass
 
-pg = st.navigation([page_1, page_2, page_3])
+pg = st.navigation([page_0,page_1, page_2, page_3])
 pg.run()
